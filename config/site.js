8705989e63b5fd882725ecee2a193e5d36ceeb37
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-01-20 18:16:51 +0900

    SEO description added

--- config/site.js
+++ config/site.js
@@ -10,9 +10,10 @@ module.exports = {
     "bootstrap",
     "startbootstrap-agency",
   ],
-  description: "Over 40 Web Club",
+  description:
+    "Web開発の勉強をしている40歳以上の方が、情報交換したり、助けあったり、交流を深めたりするためのオンラインコミュニティです。",
   url: "https://over40webclub.netlify.app/",
-  author: "-",
+  author: "Over 40 Web Club",
   image: "https://over40webclub.netlify.app/images/seo.jpg",
   trackingId: "UA-119418003-5",
   defaultLang: "ja",
