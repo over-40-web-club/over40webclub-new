967659af10d2d1da44819f9ecf2647f5fad823a0
Shoji Makino <pitang1965@gmail.com>
2021-12-19 17:28:22 +0900

    Fix: error about makeFAIcon

--- src/utils/makeFAIcon.js
+++ src/utils/makeFAIcon.js
@@ -2,7 +2,8 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function makeFAIcon(icon) {
+  // eslint-disable-next-line react/display-name
   return function (props) {
-    <FontAwesomeIcon icon={icon} {...props} />;
+    return <FontAwesomeIcon icon={icon} {...props} />;
   };
 }
